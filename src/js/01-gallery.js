0da349cbe0e130a8e23ed7f63fe9fe6691483d20
IgorYev <123358377+IgorYev@users.noreply.github.com>
2023-08-02 22:34:48 +0300

    update dz

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,54 +1,29 @@
 
 import { galleryItems } from './gallery-items';
 
-
-// Add imports above this line
 import SimpleLightbox from "simplelightbox";
 import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
-// Change code below this line
 
 
 const gallery = document.querySelector('.gallery');
-const customMarkup = buildMarkup(galleryItems);
 
-gallery.insertAdjacentHTML('beforeend', customMarkup);
+const buildMarkup = galleryItems.map(({ preview, original, description }) => 
+     `<li class="gallery__item">
+             <a class="gallery__link" href="${original}">
+               <img
+                 class="gallery__image"
+                   src="${preview}"
+                alt="${description}"
+                 />
+               </a>
+              </li>`
+    ).join('');
+  
+  gallery.insertAdjacentHTML('beforeend', buildMarkup);
+ 
+const lightBox = new SimpleLightbox('.gallery a', { 
+        captionsData: 'alt',
+        captionsDelay: 250,
+    });
 
-let instance = null;
-
-function buildMarkup(galleryItems) {
-  return galleryItems.map(({ preview, original, description }) => {
-    return `<li class="gallery__item">
-              <a class="gallery__link" href="${original}">
-                <img
-                  class="gallery__image"
-                  src="${preview}"
-                  data-source="${original}"
-                  alt="${description}"
-                />
-              </a>
-            </li>`;
-  }).join('');
-}
-
-function markupClick(event) {
-  event.preventDefault();
-
-  if (event.target.classList.contains('gallery__image')) {
-    const originalImageSrc = event.target.dataset.source;
-    instance = basicLightbox.create(`<img src="${originalImageSrc}">`);
-    instance.show();
-    gallery.addEventListener('keydown', handleKeyPress);
-  }
-}
-
-function handleKeyPress(event) {
-  if (event.code === 'Escape') {
-    instance.close();
-    gallery.removeEventListener('keydown', handleKeyPress);
-  }
-}
-
-gallery.addEventListener('click', markupClick);
-console.log(galleryItems);
-
-console.log(galleryItems);
+console.log(gallery);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,42 +1,37 @@
 import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-user';
+
 const refs = {
     form: document.querySelector('.feedback-form'),
     textarea: document.querySelector('.feedback-form textarea'),
     input: document.querySelector('.feedback-form input')
 };
+const formState = throttle(() => {
+  const email = refs.input.value;
+  const message = refs.textarea.value;
+  localStorage.setItem('STORAGE_KEY', JSON.stringify({ email, message }));
+}, 500);
 
 refs.form.addEventListener('submit', onFormSubmit);
-refs.textarea.addEventListener('input', onTextareaInput);
+refs.textarea.addEventListener('input', formState); 
+refs.input.addEventListener('input', formState); 
 
-const formState = throttle(() => {
-    const email = refs.input.value;
-    const message = refs.textarea.value;
-    localStorage.setItem('feedback-form-user', JSON.stringify({email, message}));
-  }, 500);
-
-  function restoreFormState() {
-    const savedFormState = localStorage.getItem('feedback-form-user');
+function restoreFormState() {
+    const savedFormState = localStorage.getItem('STORAGE_KEY');
     if (savedFormState) {
-      const formState = JSON.parse(savedFormState);
-      refs.input.value = formState.email;
-      refs.textarea.value = formState.message;
+        const formState = JSON.parse(savedFormState);
+        refs.input.value = formState.email;
+        refs.textarea.value = formState.message;
     }
-  }
+}
 
-  function onTextareaInput() {
-    formState();
-  }
-
-  function onFormSubmit(event) {
+function onFormSubmit(event) {
     event.preventDefault();
-    const formState = JSON.parse(localStorage.getItem('feedback-form-user'));
+    const formState = JSON.parse(localStorage.getItem('STORAGE_KEY'));
     console.log(formState);
-    localStorage.removeItem('feedback-form-user');
+    localStorage.removeItem('STORAGE_KEY');
     refs.form.reset();
-  }
+}
 
-  refs.form.addEventListener('submit', onFormSubmit);
-  refs.textarea.addEventListener('input', onTextareaInput);
-
-  restoreFormState();
+restoreFormState();
